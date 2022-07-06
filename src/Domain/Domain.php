c58c4c6ea6e55d353cf23fd392d32d391909cd00
Vitaliy <theyamshikov@yandex.ru>
2022-07-06 18:44:37 +0600

    upd

--- src/Domain/Domain.php
+++ src/Domain/Domain.php
@@ -2,7 +2,7 @@
 
 namespace ZnBundle\Person\Domain;
 
-use ZnCore\Domain\Domain\Interfaces\DomainInterface;
+use ZnCore\Domain\Interfaces\DomainInterface;
 
 class Domain implements DomainInterface
 {
